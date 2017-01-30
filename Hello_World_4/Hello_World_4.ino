380b711f97b076cdb1f6de1ef44f8786cc75a919
edderAri <edder.1022@gmail.com>
2017-01-30 20:49:01 +0100

    Prog_4

--- Hello_World_4/Hello_World_4.ino
+++ Hello_World_4/Hello_World_4.ino
@@ -16,9 +16,10 @@ void setup() {               // configura el final de salida
   Serial.begin(9600);   /* Configurar una biblioteca serie a 9600 
   bps*/
   Serial.println("Escull el numero de l'operacio que vols realitzar?");  
-  Serial.println("1.  Comprovar numero de tarjeta de credit");
-  Serial.println("2.  Comprovar numero de compte bancari");
-  Serial.println("3.  Buscar un digit perdut de tarjeta de credit");
+  Serial.println("  1. Comprovar numero de tarjeta de credit");
+  Serial.println("  2. Comprovar numero de compte bancari");
+  Serial.println("  3. Buscar un digit perdut de tarjeta de credit");
+  /*Aquests missatjes surten en vertical*/
 }
 
 //************************** LOOP *******************************
