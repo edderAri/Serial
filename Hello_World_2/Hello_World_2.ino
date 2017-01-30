7604bec18cf819544d28f8d0975cd4015c0dd693
edderAri <edder.1022@gmail.com>
2017-01-30 20:16:57 +0100

    Prog_2

--- Hello_World_2/Hello_World_2.ino
+++ Hello_World_2/Hello_World_2.ino
@@ -21,7 +21,7 @@ void setup() {               // configura el final de salida
 //************************** LOOP *******************************
 
 void loop() {                // inicia el bucle del programa
-  Serial.println("Hello world!"); /* Hola mundo!*/ 
+  Serial.print("Hello world!"); /* Hola mundo!*/ 
   delay(1000); /*Repeteix el missatge al minitor cada segon*/
  
 }
