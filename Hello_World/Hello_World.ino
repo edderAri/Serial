3e8f8b48b7a59e0f54aa06ba9026fc1b7f27615a
edderAri <edder.1022@gmail.com>
2017-01-30 20:04:26 +0100

    Hello_World_setup

---
+++ Hello_World/Hello_World.ino
@@ -0,0 +1,29 @@
+/****************************************************************
+**                                                             **
+**                           TÍTOL: Serial                     **
+**                                                             **
+**  NOM: EDDER GARCIA PINO                     DATA 30/01/2017 **
+****************************************************************/
+//************************ INCLUDE ******************************
+
+
+//*********************** VARIABLES *****************************
+
+
+//************************* SETUP *******************************
+
+void setup() {               // configura el final de salida
+  Serial.begin(9600);   /* Configurar una biblioteca serie a 9600 
+  bps*/
+  Serial.println("Hello world!"); /* Hola mundo! El monitor 
+  esa frase una vez; setup*/ 
+ 
+}
+
+//************************** LOOP *******************************
+
+void loop() {                // inicia el bucle del programa
+
+}
+
+//************************* FUNCIONS ****************************
