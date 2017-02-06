66df7262fcd832637f63ecfd684684173a909880
edderAri <edder.1022@gmail.com>
2017-02-06 19:56:00 +0100

    Prog_4

--- Math_Is_Fun_4/Math_Is_Fun_4.ino
+++ Math_Is_Fun_4/Math_Is_Fun_4.ino
@@ -19,15 +19,20 @@ void setup() {
   Serial.begin(9600);
   
   Serial.print("Your HD is ");  /*Tu Disco duro es*/
-  Serial.print(drive_gb); /*5*/
+  Serial.print(drive_gb); /*100*/
   Serial.println(" GB large."); /*GB grande*/
 
-  drive_mb = 1024 * drive_gb; /*1024 * 5*/
+  drive_mb = 1024 * drive_gb; /*1024 * 100*/
 
   Serial.print("It can store "); /*puede almacenar*/
-  Serial.print(drive_mb); /* 5120=(1024*5) */
+  Serial.print(drive_mb); /* -28672=(1024*5) */
   Serial.println(" Megabytes!");
-  
+  /* El int drive_mb s'ha de canviar per long drive_mb Las variables 
+largas son variables de tamaño extendidas para el almacenamiento de 
+números y almacenan 32 bits (4 bytes), de -2.147.483.648 a 
+2.147.483.647.
+Si hace matemáticas con enteros, al menos uno de los números debe 
+ser seguido por un L, forzándolo a ser un largo.*/
 }
 
 //************************** LOOP *******************************
