76d658c249ef7e693a7ae6882e3efb09f77db525
edderAri <edder.1022@gmail.com>
2017-02-06 20:10:12 +0100

    Prog_5

--- Math_Is_Fun_5/Math_Is_Fun_5.ino
+++ Math_Is_Fun_5/Math_Is_Fun_5.ino
@@ -28,6 +28,11 @@ void setup() {
   Serial.print(drive_mb); /* 102400=(1024*100) */
   Serial.println(" Megabytes!");
   
+/*Las variables largas son variables de tamaño extendidas para 
+el almacenamiento de números y almacenan 32 bits (4 bytes), 
+de -2.147.483.648 a 2.147.483.647.
+Si hace matemáticas con enteros, al menos uno de los números debe 
+ser seguido por un L, forzándolo a ser un largo.*/
 }
 
 //************************** LOOP *******************************
