c5f2d4da92090ba825f1f9d7ede3c6d9997791ec
edderAri <edder.1022@gmail.com>
2017-02-13 21:12:59 +0100

    Prog_2

---
+++ Print_2/Print_2.ino
@@ -0,0 +1,47 @@
+/****************************************************************
+**                                                             **
+**                           TÍTOL: Serial                     **
+**                                                             **
+**  NOM: EDDER GARCIA PINO                     DATA 30/01/2017 **
+****************************************************************/
+//************************ INCLUDE ******************************
+
+
+//*********************** VARIABLES *****************************
+
+int thisByte = 33;
+
+//************************* SETUP *******************************
+
+void setup() 
+{              
+  Serial.begin(9600); 
+  Serial.println("ASCII Table ~ Character Map");
+
+}
+
+//************************** LOOP *******************************
+
+void loop()  // inicia el bucle del programa
+{               
+  Serial.write(thisByte);    
+  
+  Serial.print(", dec: "); 
+  Serial.print(thisByte);      
+  Serial.print(", hex: "); 
+  Serial.print(thisByte, HEX);     
+  Serial.print(", oct: "); 
+  Serial.print(thisByte, OCT);     
+  Serial.print(", bin: "); 
+  Serial.println(thisByte, BIN);   
+
+  if(thisByte == 126)  // if printed last visible character '~'
+  { 
+    while(true)
+    { 
+    } 
+  } 
+  thisByte++;     // go on to the next character
+}
+
+//************************* FUNCIONS ****************************
