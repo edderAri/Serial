d09ed09f9ef70df8d27ba6623fe33e3e96de53c6
edderAri <edder.1022@gmail.com>
2017-04-11 19:49:47 +0200

    f

--- Control_Structures_8/Control_Structures_8.ino
+++ Control_Structures_8/Control_Structures_8.ino
@@ -14,18 +14,17 @@ int comptar = 11;
 
 void setup()
 {
-  Serial.begin(9600);       // set up Serial library at 9600 bps
+  Serial.begin(9600);     // set up Serial library at 9600 bps
   Serial.print("Ara comptare de 0 a ");
   Serial.println(comptar);
-  for (int i = 0; i <= comptar; i++)  /*Incrementa (i++) del 0 (i=0) al valor de la variable (<= comptar)*/
+  while (i  <= comptar);
   {
-    Serial.print(i); /*imprimeix el valor de i cada cop que torna a fer el bucle*/
-    if ( i <= (comptar - 1)) /*si i es menor o igual q la variable resta 1 y no posa el guio*/
-       {
-      Serial.print("-"); /*imprimeix el - cada cop que torna a fer el bucle*/
-    }
+    Serial.print(i);
+    Serial.print("-");
+    i++;
   }
 }
+
 
 
 
